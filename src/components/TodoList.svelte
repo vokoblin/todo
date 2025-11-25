adf7dedaa1ca4e7c32900850e6a8e53cefb3fea3
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2025-11-25 02:52:28 +0000

    Making each TODO expand individually

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -58,7 +58,7 @@
       </button>
     </div>
   {:else}
-    <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start">
       {#each projects as project (project.id)}
         <ProjectCard {project} />
       {/each}
